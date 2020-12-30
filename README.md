d4284861a69212c9ee26e1d8dc76b1ef43e5bde4
Your Name <you@example.com>
2020-12-30 17:27:07 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bulk Sell or Disassemble Alcohol
+Removes all alcohol from backpack and gives equivalent in Mats or Eddies 
+
+![](demo.gif)
+
+
+## Install & Run
+1) Install [Cyber Engine Tweaks](https://github.com/yamashi/CyberEngineTweaks)
+2) Copy lua files to bin/x64/plugins/cyber_engine_tweaks/scripts/
+3) To get materials:
+    `dofile "bin/x64/plugins/cyber_engine_tweaks/scripts/disassembleAlcohol.lua"`
+4) To get €$: `dofile "bin/x64/plugins/cyber_engine_tweaks/scripts/sellAlcohol.lua"`
---
+++ sellAlcohol.lua
@@ -0,0 +1,64 @@
+-- dofile "bin/x64/plugins/cyber_engine_tweaks/scripts/sellAlcohol.lua"
+
+-- All alcohol sells for €$ 30, script counts and deletes all alcohol in backpack
+-- then adds 30*counts eddies to inventory 
+
+alcohol = {
+"Items.LowQualityAlcohol1",
+"Items.LowQualityAlcohol2",
+"Items.LowQualityAlcohol3",
+"Items.LowQualityAlcohol4",
+"Items.LowQualityAlcohol5",
+"Items.LowQualityAlcohol6",
+"Items.LowQualityAlcohol7",
+"Items.LowQualityAlcohol8",
+"Items.LowQualityAlcohol9",
+"Items.MediumQualityAlcohol1",
+"Items.MediumQualityAlcohol2",
+"Items.MediumQualityAlcohol3",
+"Items.MediumQualityAlcohol4",
+"Items.MediumQualityAlcohol5",
+"Items.MediumQualityAlcohol6",
+"Items.MediumQualityAlcohol7",
+"Items.GoodQualityAlcohol1",
+"Items.GoodQualityAlcohol2",
+"Items.GoodQualityAlcohol3",
+"Items.GoodQualityAlcohol4",
+"Items.GoodQualityAlcohol5",
+"Items.GoodQualityAlcohol6",
+"Items.NomadsAlcohol1",
+"Items.NomadsAlcohol2",
+"Items.TopQualityAlcohol1",
+"Items.TopQualityAlcohol2",
+"Items.TopQualityAlcohol3",
+"Items.TopQualityAlcohol4",
+"Items.TopQualityAlcohol5",
+"Items.TopQualityAlcohol6",
+"Items.TopQualityAlcohol7"
+}
+
+ts = Game.GetTransactionSystem()
+player = Game.GetPlayer()
+TotalAlcohol = 0 
+
+for alcoholCount = 1, #alcohol do
+    tid = TweakDBID.new(alcohol[alcoholCount])
+    itemid = ItemID.new(tid)
+    quantity = ts:GetItemQuantity(player,itemid)
+    TotalAlcohol = TotalAlcohol + quantity --append to total
+    ts:RemoveItemByTDBID(player, tid, quantity) --remove all of the type
+end
+print("Total Alcohol in inventory: " .. tostring(TotalAlcohol))
+
+
+
+eddies = ItemID.new(TweakDBID.new("Items.money"))
+result = ts:GiveItem(player, eddies, 30*TotalAlcohol)
+if result then
+    print("Obtained " .. tostring(5*TotalAlcohol) .. " Eddies")
+else
+    print("Failed to obtained " .. tostring(5*TotalAlcohol) .. " Eddies")
+end
+
+userInput = read()
+print(userInput)
